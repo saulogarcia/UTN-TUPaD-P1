960417768b7ed8f0c7ab3d013e18cc7add4082fe
saulo <saulolazarogarcia@gmail.com>
2025-11-28 16:12:20 -0300

    ejercicios archivos

--- 08-archivos/tp-archivos.py
+++ 08-archivos/tp-archivos.py
@@ -1,11 +1,11 @@
 import os
 nombre_archivo = "productos.txt"
-mis_productos = [
-    "Mouse,150.50,100",
-    "Fuente,1085.00,50",
-    "Monitor,12000.00,20"
+mis_productos_iniciales = [
+    {"nombre": "Mouse", "precio": 150.50, "cantidad": 100},
+    {"nombre": "Fuente", "precio": 1085.00, "cantidad": 50},
+    {"nombre": "Monitor", "precio": 12000.00, "cantidad": 20}
 ]
-productos_lista_diccionarios = []
+mis_productos = []
 
 # 1. Crear archivo inicial con productos: Crear un archivo de texto llamado
 # productos.txt con tres productos. Cada línea debe tener: nombre,precio,cantida
@@ -16,7 +16,7 @@ def crear_archivo(nombre_archivo, mis_productos):
     else:
         with open(nombre_archivo, "w") as archivo:
             for producto in mis_productos:
-                archivo.write(producto + "\n")
+                archivo.write(f"{producto['nombre']},{producto['precio']},{producto['cantidad']}\n")
         print("El archivo ha sido creado exitosamente.")
 
 # 2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada
@@ -24,49 +24,49 @@ def crear_archivo(nombre_archivo, mis_productos):
 # formato:
 # Producto: Lapicera | Precio: $120.5 | Cantidad: 30
 
-def leer_mostrar_productos(nombre_archivo):
+def leer_mostrar_productos(nombre_archivo, mis_productos):
+    mis_productos.clear()
     with open(nombre_archivo, "r") as archivo:
         for linea in archivo:
             producto = linea.strip().split(",")
+            # agregar el producto a la lista diccionario
+            mis_productos.append({"nombre": producto[0], "precio": producto[1], "cantidad": producto[2]})
             print(f"Producto: {producto[0]:>10} | Precio: {producto[1]:>10} | Cantidad: {producto[2]:>5}")
+    print("---" * 20)
+    print("")
+    return mis_productos
 
 # 3. Agregar productos desde teclado: Modificar el programa para que luego de mostrar
 # los productos, le pida al usuario que ingrese un nuevo producto (nombre, precio,
 # cantidad) y lo agregue al archivo sin borrar el contenido existente.
 
-def cargar_nuevo_producto(productos_lista_diccionarios, nombre_archivo):
+def cargar_nuevo_producto(mis_productos, nombre_archivo):
     nuevo_producto = input("Ingrese el nombre del nuevo producto: ")
     nuevo_precio = float(input("Ingrese el precio del nuevo producto: "))
     nuevo_cantidad = int(input("Ingrese la cantidad del nuevo producto: "))
     # agregar el nuevo producto a la lista
-    productos_lista_diccionarios.append({"nombre": nuevo_producto, "precio": nuevo_precio, "cantidad": nuevo_cantidad})
+    mis_productos.append({"nombre": nuevo_producto, "precio": nuevo_precio, "cantidad": nuevo_cantidad})
     # guardar el nuevo producto en el archivo
     with open(nombre_archivo, "a") as archivo:
         archivo.write(f"{nuevo_producto},{nuevo_precio},{nuevo_cantidad}\n")
     print(f"Producto {nuevo_producto} agregado exitosamente.")
-    return productos_lista_diccionarios
 
 
 # 4. Cargar productos en una lista de diccionarios: Al leer el archivo, cargar los datos en
 # una lista llamada productos, donde cada elemento sea un diccionario con claves:
 # nombre, precio, cantidad.
 
-def cargar_productos_en_lista(nombre_archivo, productos_lista_diccionarios):
-    with open(nombre_archivo, "r") as archivo:
-        for linea in archivo:
-            producto = linea.strip().split(",")
-            productos_lista_diccionarios.append({"nombre": producto[0], "precio": producto[1], "cantidad": producto[2]})
-    return productos_lista_diccionarios
+# resuelto en el punto 2
 
 # 5. Buscar producto por nombre: Pedir al usuario que ingrese el nombre de un
 # producto. Recorrer la lista de productos y, si lo encuentra, mostrar todos sus datos. Si
 # no existe, mostrar un mensaje de error.
 
-def buscar_producto(productos_lista_diccionarios):
-    nombre_producto = input("Ingrese el nombre del producto a buscar: ")
-    for producto in productos_lista_diccionarios:
-        if producto["nombre"] == nombre_producto:
-            print(producto)
+def buscar_producto(mis_productos):
+    nombre_producto = input("Ingrese el nombre del producto a buscar: ").lower().strip()
+    for producto in mis_productos:
+        if producto["nombre"].lower() == nombre_producto:
+            print(f"Producto encontrado: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
             break
     else:
         print("Producto no encontrado.")
@@ -75,9 +75,9 @@ def buscar_producto(productos_lista_diccionarios):
 # productos, sobrescribir el archivo productos.txt escribiendo nuevamente todos los
 # productos actualizados desde la lista.
 
-def guardar_productos_actualizados(nombre_archivo, productos_lista_diccionarios):
+def guardar_productos_actualizados(nombre_archivo, mis_productos):
     with open(nombre_archivo, "w") as archivo:
-        for producto in productos_lista_diccionarios:
+        for producto in mis_productos:
             archivo.write(f"{producto['nombre']},{producto['precio']},{producto['cantidad']}\n")
     print("Productos actualizados guardados exitosamente.")
 
@@ -86,33 +86,30 @@ def guardar_productos_actualizados(nombre_archivo, productos_lista_diccionarios)
 def menu():
     while True:
         print("")
+        print("---" * 20)
+        print("MENU")
+        print("---" * 20)
         print("1. Crear archivo inicial con productos")
-        print("2. Leer y mostrar productos")
-        print("3. Agregar productos desde teclado")
-        print("4. Cargar productos en una lista de diccionarios")
-        print("5. Buscar producto por nombre")
-        print("6. Guardar los productos actualizados")
-        print("7. Salir")
+        print("2. Leer y mostrar productos (Cargar productos en una lista de diccionarios)")
+        print("3. Mostrar y agregar productos desde teclado")
+        print("4. Buscar producto por nombre")
+        print("5. Guardar los productos en el archivo desde la lista de diccionarios")
+        print("6. Salir")
+        print("")
         opcion = int(input("Ingrese el numero de la opcion: "))
+        print("---" * 20)
         match opcion:
             case 1:
-                crear_archivo(nombre_archivo, mis_productos)
-                productos_lista_diccionarios = cargar_productos_en_lista(nombre_archivo, productos_lista_diccionarios)
+                crear_archivo(nombre_archivo, mis_productos_iniciales)
             case 2:
-                leer_mostrar_productos(nombre_archivo)
+                leer_mostrar_productos(nombre_archivo,mis_productos)
             case 3:
-                productos_lista_diccionarios = cargar_nuevo_producto(productos_lista_diccionarios, nombre_archivo)
+                cargar_nuevo_producto(leer_mostrar_productos(nombre_archivo,mis_productos), nombre_archivo)
             case 4:
-                productos_lista_diccionarios = cargar_productos_en_lista(nombre_archivo, productos_lista_diccionarios)
-                print(productos_lista_diccionarios)
+                buscar_producto(mis_productos)
             case 5:
-                if len(productos_lista_diccionarios) == 0:
-                    print("Primero debe cargar productos en una lista de diccionarios.")
-                else:
-                    buscar_producto(productos_lista_diccionarios)
+                guardar_productos_actualizados(nombre_archivo, mis_productos)
             case 6:
-                guardar_productos_actualizados(nombre_archivo, productos_lista_diccionarios)
-            case 7:
                 print("Adios")
                 break
             case _:
